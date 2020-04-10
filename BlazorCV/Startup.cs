eea284d0bb915e086680072216293632f8bbad80
viktor111 <swifrorlilko@gmail.com>
2020-04-10 16:22:49 +0300

    changes to intial project

--- BlazorCV/Startup.cs
+++ BlazorCV/Startup.cs
@@ -39,7 +39,6 @@ namespace BlazorCV
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
